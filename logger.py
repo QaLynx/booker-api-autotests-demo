6baf5c9c31666d7d08b6d87e97a8060c26e79860
QaLynx <kirdeevvital.qa@gmail.com>
2025-06-04 18:30:56 +0300

    add logger

---
+++ logger.py
@@ -0,0 +1,19 @@
+import logging
+import sys
+
+def setup_logger():
+    """настройки логера"""
+    logger = logging.getLogger(__name__)
+    logger.setLevel(logging.DEBUG)
+
+    console_handler = logging.StreamHandler(sys.stdout)
+    console_handler.setLevel(logging.DEBUG)
+
+    formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    console_handler.setFormatter(formatter)
+
+    logger.addHandler(console_handler)
+
+    return logger
+
+logger = setup_logger()
